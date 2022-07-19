43e762e1be51a312d6160b70e4116f1f0a2622ad
SENA KURTULMUŞLU <sena.kurtulmuslu@std.yildiz.edu.tr>
2022-07-19 14:58:48 +0300

    Add project files.

---
+++ TikTakToeGame/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TikTakToeGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] gameBoard = new string[9];
+        int currentTurn = 0;
+
+        public string returnSymbol(int turn)
+        {
+            if (turn %2 == 0)
+            {
+                return "O";
+            }
+            else
+            {
+                return "X";
+            }
+        }
+
+        public void checkForWinner()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                string combination = "";
+
+                switch (i)
+                {
+                    case 0:
+                        combination = gameBoard[0] + gameBoard[4] + gameBoard[8];
+                        break;
+                    case 1:
+                        combination = gameBoard[2] + gameBoard[4] + gameBoard[6];
+                        break;
+                    case 2:
+                        combination = gameBoard[0] + gameBoard[1] + gameBoard[2];
+                        break;
+                    case 3:
+                        combination = gameBoard[3] + gameBoard[4] + gameBoard[5];
+                        break;
+                    case 4:
+                        combination = gameBoard[6] + gameBoard[7] + gameBoard[8];
+                        break;
+                    case 5:
+                        combination = gameBoard[0] + gameBoard[3] + gameBoard[6];
+                        break;
+                    case 6:
+                        combination = gameBoard[1] + gameBoard[4] + gameBoard[7];
+                        break;
+                    case 7:
+                        combination = gameBoard[2] + gameBoard[5] + gameBoard[8];
+                        break;
+                }
+
+                if (combination.Equals("XXX"))
+                {
+                    MessageBox.Show("X has won the game!", "We have a winner", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    reset();
+                }
+                else if (combination.Equals("OOO"))
+                {
+                    MessageBox.Show("O has won the game!", "We have a winner", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    reset();
+                }
+                checkDraw();
+            }
+        }
+
+        public void checkDraw()
+        {
+            int counter = 0;
+
+            for (int i = 0; i < gameBoard.Length; i++)
+            {
+                if (gameBoard[i] != null) { counter++; }
+
+                if (counter == 9)
+                {
+                    reset();
+                    MessageBox.Show("No winner, play again", "OUUPS!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        public void reset()
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+
+            gameBoard = new string[9];
+            currentTurn = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[0] = returnSymbol(currentTurn);
+            button1.Text = gameBoard[0];
+            checkForWinner();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[1] = returnSymbol(currentTurn);
+            button2.Text = gameBoard[1];
+            checkForWinner();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[2] = returnSymbol(currentTurn);
+            button3.Text = gameBoard[2];
+            checkForWinner();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[3] = returnSymbol(currentTurn);
+            button4.Text = gameBoard[3];
+            checkForWinner();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[4] = returnSymbol(currentTurn);
+            button5.Text = gameBoard[4];
+            checkForWinner();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[5] = returnSymbol(currentTurn);
+            button6.Text = gameBoard[5];
+            checkForWinner();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[6] = returnSymbol(currentTurn);
+            button7.Text = gameBoard[6];
+            checkForWinner();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[7] = returnSymbol(currentTurn);
+            button8.Text = gameBoard[7];
+            checkForWinner();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[8] = returnSymbol(currentTurn);
+            button9.Text = gameBoard[8];
+            checkForWinner();
+        }
+    }
+}
